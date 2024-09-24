624f31dcee0c552e316d8bf970353a72c319c463
chicho377 <morafabricio86@gmail.com>
2024-09-24 01:17:01 -0600

    nueva mejora metodo menu para facilitar la vista

--- Codigo Tarea2/Main/src/main/Main.java
+++ Codigo Tarea2/Main/src/main/Main.java
@@ -12,12 +12,17 @@ import java.util.Scanner;
  * @author Fabricio Mora Gomez
  */
 public class Main {
+    public static void menu(){
+          Scanner scanner = new Scanner(System.in);
+          int opcion;
+    }
+    
     public static void main(String[] args) {
         /* Profe para practicar el scanner lo hice que hay que ingresar los datos
         entonces le voy a dejar la lista de agentes.
     Listado de agentes y sus roles en Valorant:
 
-    Duelistas (Ataque agresivo): (6 puntos requeridos para la definitiva)
+    rol = Duelistas (Ataque agresivo): (6 puntos requeridos para la definitiva)
     1. Jett 
     2. Phoenix
     3. Reyna
@@ -25,7 +30,7 @@ public class Main {
     5. Yoru
     6. Neon
 
-    Iniciadores (Abren espacios y apoyan al equipo): (7 puntos requeridos para la definitiva)
+    rol = Iniciadores (Abren espacios y apoyan al equipo): (7 puntos requeridos para la definitiva)
     1. Breach
     2. Sova
     3. Skye
@@ -33,20 +38,19 @@ public class Main {
     5. Fade
     6. Gekko
 
-    Centinelas (Defienden y protegen al equipo): (8 puntos requeridos para la definitiva)
+    rol = Centinelas (Defienden y protegen al equipo): (8 puntos requeridos para la definitiva)
     1. Sage
     2. Cypher
     3. Killjoy
     4. Chamber
 
-    Controladores (Controlan el mapa y bloquean visión): (7 puntos requeridos para la definitiva)
+    rol = Controladores (Controlan el mapa y bloquean visión): (7 puntos requeridos para la definitiva)
     1. Brimstone
     2. Viper
     3. Omen
     4. Astra
     5. Harbor
 */
-
         
         // variables
         String nombre;
@@ -62,18 +66,22 @@ public class Main {
         nombre = scanner.nextLine();
         System.out.println("Ingrese el rol del agente seleccionado: ");
         rol = scanner.nextLine();
-        System.out.println("Ingrese los puntos actuales de la habilidad definitiva: ");
+        System.out.println("Ingrese los puntos actuales que posee de la habilidad definitiva: ");
         habilidadUltimaCarga = scanner.nextInt();
         
         // crear objeto del agente
         Agente agente = new Agente(nombre, rol, habilidadUltimaCarga);
         
+        // objeto sin scanner
+        Agente jett = new Agente("Jett", "Duelista", 4);
+        
         // mostrar detalles del agente
         agente.mostrarDetalles();
+        jett.mostrarDetalles();
         
         // intentar usar habilidad definitiva
         agente.usarUltima();
-        
+        jett.usarUltima();
         
         // pedir puntos a cargar
         System.out.println("Ingrese los puntos a cargar: ");
@@ -82,9 +90,11 @@ public class Main {
         
         // cargar mas puntos a la habilidad definitiva
         agente.cargarUltima(puntos);
+        jett.cargarUltima(2);
         
         // intentar usar la habilidad definitiva nuevamente
         agente.usarUltima();
+        jett.usarUltima();
     }
     
 }
