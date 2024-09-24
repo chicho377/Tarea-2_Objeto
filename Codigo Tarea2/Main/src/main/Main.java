52592f891e5af7baf03cd17d74dac2281322f0b9
chicho377 <morafabricio86@gmail.com>
2024-09-24 00:00:18 -0600

    Agregando documento y creacion de proyecto main y clase agente

---
+++ Codigo Tarea2/Main/src/main/Main.java
@@ -0,0 +1,16 @@
+/*
+ Tarea #2
+ Progra II
+    Forme una clase separada del "main" y cree cualquier objeto cualquiera
+    con minimo 3 atributos y dos metodos.
+ */
+package main;
+/**
+ * @author Fabricio Mora Gomez
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+    
+}
